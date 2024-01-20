7427a5b6374149622b52e5547d77be0cc6dee13b
gsw <ggssww99@gmail.com>
2024-01-20 10:05:07 +0900

    JUnit5 테스트 반복하기2

--- src/test/java/com/whiteship/javatestcode/StudyTest.java
+++ src/test/java/com/whiteship/javatestcode/StudyTest.java
@@ -1,8 +1,16 @@
 package com.whiteship.javatestcode;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,9 +42,31 @@ class StudyTest {
 
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있어요."})
-    void parameterizedTest (String message){
-        System.out.println("message = " + message);
+    //@ValueSource(strings = {"날씨가", "많이", "추워지고", "있어요."})
+    //@ValueSource(ints = {10, 20, 40})
+    //@EmptySource
+    //@NullSource
+    //@NullAndEmptySource
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void parameterizedTest (@AggregateWith(StudyAggregator.class) Study study){
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator{
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter{
+
+        @Override
+        protected Object convert(Object o, Class<?> aClass) throws ArgumentConversionException {
+            Assertions.assertEquals(Study.class, aClass, "Can only convert to Study");
+            return new Study(Integer.parseInt(o.toString()));
+        }
     }
 
     @BeforeAll
